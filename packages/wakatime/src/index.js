876dcfbe6ed4bfa81273c33598f05bdc80a9b159
sunss <sss213018@163.com>
2021-11-20 13:26:36 +0800

    🐛 fix js

--- packages/wakatime/src/index.js
+++ packages/wakatime/src/index.js
@@ -1,9 +1,13 @@
 // 获取环境变量
 import dotenv from "dotenv";
 dotenv.config();
+// Github API 工具：Octokit
+import Octokit from "@octokit/rest";
 // fetch
 import fetch from "node-fetch";
-import { updateGist } from "./util.js";
+import { trimRightStr, generateBarChart } from "./util.js";
+
+const octokit = new Octokit({ auth: `token ${githubToken}` });
 
 // 常量
 // https://wakatime.com/developers
@@ -13,6 +17,52 @@ const { GIST_ID: gistId, GH_TOKEN: githubToken, WAKATIME_API_KEY: wakatimeApiKey
 // Key 需要经过 base64 编码
 const Authorization = `Basic ${Buffer.from(wakatimeApiKey).toString("base64")}`;
 
+// updateGist
+export async function updateGist(stats) {
+  let gist;
+  try {
+    gist = await octokit.gists.get({ gist_id: gistId });
+  } catch (error) {
+    console.error(`Unable to get gist\n${error}`);
+  }
+
+  const lines = [];
+  for (let i = 0; i < Math.min(stats.data.languages.length, 5); i++) {
+    const data = stats.data.languages[i];
+    const { name, percent, text: time } = data;
+
+    const line = [
+      trimRightStr(name, 10).padEnd(10),
+      time.padEnd(14),
+      generateBarChart(percent, 21),
+      String(percent.toFixed(1)).padStart(5) + "%",
+    ];
+
+    lines.push(line.join(" "));
+
+    console.log("lines", lines);
+  }
+
+  if (lines.length == 0) return;
+
+  try {
+    // Get original filename to update that same file
+    const filename = Object.keys(gist.data.files)[0];
+    await octokit.gists.update({
+      gist_id: gistId,
+      files: {
+        [filename]: {
+          filename: `📊 Weekly development breakdown`,
+          content: lines.join("\n"),
+        },
+      },
+    });
+  } catch (error) {
+    console.error(`Unable to update gist\n${error}`);
+  }
+}
+
+// 执行主函数
 async function main() {
   try {
     const response = await fetch(URL, {
@@ -24,8 +74,7 @@ async function main() {
     });
     const stats = await response.json();
     console.log("请求成功：", stats);
-    console.log("updateGist===>");
-    await updateGist(stats, gistId, githubToken);
+    await updateGist(stats);
     console.log("完成===>");
   } catch (error) {
     console.log("请求失败：", error);
--- packages/wakatime/src/util.js
+++ packages/wakatime/src/util.js
@@ -1,55 +1,8 @@
-// Github API 工具：Octokit
-import Octokit from "@octokit/rest";
 // const Octokit = require("@octokit/rest");
 
 function trimRightStr(str, len) {
   // Ellipsis takes 3 positions, so the index of substring is 0 to total length - 3.
   return str.length > len ? str.substring(0, len - 3) + "..." : str;
-}
-
-export async function updateGist(stats, gistId, githubToken) {
-  let gist;
-  try {
-    const octokit = new Octokit({ auth: `token ${githubToken}` });
-    gist = await octokit.gists.get({ gist_id: gistId });
-  } catch (error) {
-    console.error(`Unable to get gist\n${error}`);
-  }
-
-  const lines = [];
-  for (let i = 0; i < Math.min(stats.data.languages.length, 5); i++) {
-    const data = stats.data.languages[i];
-    const { name, percent, text: time } = data;
-
-    const line = [
-      trimRightStr(name, 10).padEnd(10),
-      time.padEnd(14),
-      generateBarChart(percent, 21),
-      String(percent.toFixed(1)).padStart(5) + "%",
-    ];
-
-    lines.push(line.join(" "));
-
-    console.log("lines", lines);
-  }
-
-  if (lines.length == 0) return;
-
-  try {
-    // Get original filename to update that same file
-    const filename = Object.keys(gist.data.files)[0];
-    await octokit.gists.update({
-      gist_id: gistId,
-      files: {
-        [filename]: {
-          filename: `📊 Weekly development breakdown`,
-          content: lines.join("\n"),
-        },
-      },
-    });
-  } catch (error) {
-    console.error(`Unable to update gist\n${error}`);
-  }
 }
 
 function generateBarChart(percent, size) {
@@ -66,3 +19,5 @@ function generateBarChart(percent, size) {
     .join("")
     .padEnd(size, syms.substring(0, 1));
 }
+
+export { trimRightStr, generateBarChart };
